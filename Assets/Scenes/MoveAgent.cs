0b06ed654349ac45532f4bb4d2da994a1974b094
rallerboy <rallerboy100@gmail.com>
2019-12-11 14:05:10 +0100

    agent

---
+++ Assets/Scenes/MoveAgent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveAgent : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform targetPoint;
+
+    void Update()
+    {
+        agent.SetDestination(targetPoint.position);
+    }
+
+    /*
+    public void TargetLocked()
+    {
+        agent.SetDestination(targetPoint.position);
+    }
+    */
+}
+
